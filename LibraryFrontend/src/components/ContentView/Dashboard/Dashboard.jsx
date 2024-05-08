5c232a495bcdb416af7b2c641550a754a88f82c4
Marit Kristine Moe <marit.moe2@ntebb.no>
2024-05-08 10:03:59 +0200

    Display error message if any, new Books header

--- LibraryFrontend/src/components/ContentView/Dashboard/Dashboard.jsx
+++ LibraryFrontend/src/components/ContentView/Dashboard/Dashboard.jsx
@@ -3,13 +3,16 @@ import PropTypes from "prop-types";
 // Include: some facts about the library and a BookList component
 
 function Dashboard({ data, error }) {
-  console.log(data);
   return (
-    <ul>
-      {data.map((book) => (
-        <li key={book.id}>{book.title}</li>
-      ))}
-    </ul>
+    <>
+      {error && <p>{error.message}</p>}
+      {data && <h2>Books</h2>}
+      <ul>
+        {data.map((book) => (
+          <li key={book.id}>{book.title}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
