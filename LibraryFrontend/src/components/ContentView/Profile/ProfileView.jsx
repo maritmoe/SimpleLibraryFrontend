7aee485664da3ce2c679b08e6d05ac1f221fdd19
Marit Kristine Moe <marit.moe2@ntebb.no>
2024-05-13 09:55:59 +0200

    Simple ProfileView with hardcoded current user

--- LibraryFrontend/src/components/ContentView/Profile/ProfileView.jsx
+++ LibraryFrontend/src/components/ContentView/Profile/ProfileView.jsx
@@ -1,8 +1,46 @@
+import { useEffect, useState } from "react";
+
 function ProfileView() {
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const currentUser = 1;
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  const fetchUser = () => {
+    fetch(`http://localhost:5114/library/users/${currentUser}`)
+      .then((response) => {
+        const data = response.json();
+        if (response.status == 200) {
+          data.then((value) => {
+            setUser(value);
+            setErrorMessage(null);
+          });
+        } else {
+          data.then((value) => {
+            setErrorMessage(value);
+            setUser(null);
+          });
+        }
+      })
+      .catch((error) => setError(error));
+  };
+
   return (
-    <>
-      <h2>Hello from profile</h2>
-    </>
+    <div>
+      {error && <p>Error: {error.message}</p>}
+      {errorMessage && <p>Error: {errorMessage}</p>}
+      {user && (
+        <div>
+          <h2>Profile</h2>
+          <p>Users ID: {user.id}</p>
+          <p>Users Name: {user.name}</p>
+        </div>
+      )}
+    </div>
   );
 }
 
