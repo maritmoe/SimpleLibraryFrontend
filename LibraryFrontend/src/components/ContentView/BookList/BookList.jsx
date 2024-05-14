0592e66118f9e9a20451e0b1a422483948fc5224
Marit Kristine Moe <marit.moe2@ntebb.no>
2024-05-14 14:30:48 +0200

    Added sorting possibility to BookList

--- LibraryFrontend/src/components/ContentView/BookList/BookList.jsx
+++ LibraryFrontend/src/components/ContentView/BookList/BookList.jsx
@@ -5,6 +5,19 @@ import { useState } from "react";
 
 function BookList({ data, error }) {
   const [filterValue, setFilterValue] = useState("show-all");
+  const [sortValue, setSortValue] = useState("no-sorting");
+
+  const getSortedBooks = (books) => {
+    if (sortValue === "shortest") {
+      return books.sort((a, b) => a.pages - b.pages);
+    } else if (sortValue === "longest") {
+      return books.sort((a, b) => b.pages - a.pages);
+    } else if (sortValue === "title") {
+      return books.sort((a, b) =>
+        a.title > b.title ? 1 : b.title > a.title ? -1 : 0
+      );
+    } else return books;
+  };
 
   const getFilteredBooks = () => {
     if (filterValue === "long-books") {
@@ -27,12 +40,23 @@ function BookList({ data, error }) {
             value={filterValue}
             onChange={(event) => setFilterValue(event.target.value)}
           >
-            <MenuItem value="show-all">Show all</MenuItem>
-            <MenuItem value="long-books">Show long books</MenuItem>
-            <MenuItem value="short-books">Show short books</MenuItem>
+            <MenuItem value="show-all">None</MenuItem>
+            <MenuItem value="long-books">Long books</MenuItem>
+            <MenuItem value="short-books">Short books</MenuItem>
+          </Select>
+          <InputLabel id="sort-label">Sort</InputLabel>
+          <Select
+            labelId="sort-label"
+            value={sortValue}
+            onChange={(event) => setSortValue(event.target.value)}
+          >
+            <MenuItem value="no-sorting">None</MenuItem>
+            <MenuItem value="shortest">Length increasing</MenuItem>
+            <MenuItem value="longest">Length decreasing</MenuItem>
+            <MenuItem value="title">Title alphabetic</MenuItem>
           </Select>
           <List sx={{ width: "100%", maxWidth: 360 }}>
-            {getFilteredBooks().map((book) => (
+            {getSortedBooks(getFilteredBooks()).map((book) => (
               <BookListItem key={book.id} book={book} />
             ))}
           </List>
