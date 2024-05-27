3b0dfaf0e251f5a4ec3ca4780c51d94a5161d555
Marit Kristine Moe <marit.moe2@ntebb.no>
2024-05-27 14:44:26 +0200

    Fixed problem of being logged out when clicking hyperlink

--- LibraryFrontend/src/components/ContentView/Admin/UserList/UserListItem/UserListItem.jsx
+++ LibraryFrontend/src/components/ContentView/Admin/UserList/UserListItem/UserListItem.jsx
@@ -1,14 +1,12 @@
 import PropTypes from "prop-types";
 import "./UserListItem.css";
+import { Link } from "react-router-dom";
 
 function UserListItem({ user }) {
   return (
     <li className="user-list-item">
       <p>
-        User ID:{" "}
-        <a href={`/profile/${user.id}`} aria-label={`User ${user.id}`}>
-          {user.id}
-        </a>
+        User ID: <Link to={`/profile/${user.id}`}>{user.id}</Link>
       </p>
       <p className="user-name">{user.name}</p>
     </li>
